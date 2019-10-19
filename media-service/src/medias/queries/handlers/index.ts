775c1e1fbd4088b1299d049d364f312a5d9e7c1d
WhizzMirray <naimi.abdelhadi.97@gmail.com>
2019-10-19 11:38:11 +0200

    media-service/typos

--- media-service/src/medias/queries/handlers/index.ts
+++ media-service/src/medias/queries/handlers/index.ts
@@ -1,4 +1,4 @@
 import { GetMediasHandler } from './get-medias.handler';
-import { GetMediaByIdHandler } from './get-media-by-id-.handler';
+import { GetMediaByIdHandler } from './get-media-by-id.handler';
 
 export const QueryHandlers = [GetMediasHandler, GetMediaByIdHandler];

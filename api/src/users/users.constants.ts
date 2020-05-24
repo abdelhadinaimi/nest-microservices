9072921b80ca9fa8fe4c547fe2b21b3eb31ce0c8
shamim42 <shamim.cse.bd@gmail.com>
2020-05-25 00:53:07 +0600

    Withdrawn authorization from almost all api

--- api/src/users/users.constants.ts
+++ api/src/users/users.constants.ts
@@ -3,8 +3,8 @@ import { IRoute } from '../app.types';
 
 export const ROUTES: IRoute[] = [
   { path: '/users/:id', protected: false, method: RequestMethod.GET },
-  { path: '/users/:id', protected: true, method: RequestMethod.POST },
+  { path: '/users/:id', protected: false, method: RequestMethod.POST },
   { path: '/users/:id', protected: true, method: RequestMethod.PUT },
-  { path: '/users/', protected: true, method: RequestMethod.GET },
-  { path: '/users/', protected: true, method: RequestMethod.POST },
+  { path: '/users/', protected: false, method: RequestMethod.GET },
+  { path: '/users/', protected: false, method: RequestMethod.POST },
 ]

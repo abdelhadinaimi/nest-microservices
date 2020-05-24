8c8c7882059669ba581d9b8de254918277e33623
shamim42 <shamim.cse.bd@gmail.com>
2020-05-25 02:57:15 +0600

    changes as per need to run the project fluently

--- api/src/medias/medias.constants.ts
+++ api/src/medias/medias.constants.ts
@@ -3,8 +3,8 @@ import { IRoute } from '../app.types';
 
 export const ROUTES: IRoute[] = [
   { path: '/medias/:id', protected: false, method: RequestMethod.GET },
-  { path: '/medias/:id', protected: true, method: RequestMethod.POST },
+  { path: '/medias/:id', protected: false, method: RequestMethod.POST },
   { path: '/medias/:id', protected: true, method: RequestMethod.PUT },
-  { path: '/medias/', protected: true, method: RequestMethod.GET },
-  { path: '/medias/', protected: true, method: RequestMethod.POST },
+  { path: '/medias/', protected: false, method: RequestMethod.GET },
+  { path: '/medias/', protected: false, method: RequestMethod.POST },
 ]
